ebd6890293834d4e35aa08b8ce7ca09499028859
debidutta123 <165667866+debidutta123@users.noreply.github.com>
2025-09-16 23:43:37 +0530

    ok

--- course.py
+++ course.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from tkinter import ttk
 from PIL import Image, ImageTk
 
 class Course:
@@ -29,7 +30,7 @@ class Course:
         self.txt_duration = Entry(self.root, textvariable = self.var_duration, font = ("goudy old style", 15, "bold"), bg = "lightyellow").place(x = 150, y = 100, width = 200)
         self.txt_charges = Entry(self.root, textvariable = self.var_charges, font = ("goudy old style", 15, "bold"), bg = "lightyellow").place(x = 150, y = 140, width = 200)
         self.txt_description = Text(self.root, font = ("goudy old style", 15, "bold"), bg = "lightyellow")
-        self.txt_description.place(x = 150, y = 180, width = 500, height = 100)
+        self.txt_description.place(x = 150, y = 180, width = 500, height = 130)
 
         # Buttons
         self.btn_add = Button(self.root, text = "Save", font = ("goudy old style", 15, "bold"), bg = "#2196f3", fg = "white", cursor = "hand2")
@@ -40,6 +41,43 @@ class Course:
         self.btn_delete.place(x = 390, y = 400, width = 110, height = 40)
         self.btn_clear = Button(self.root, text = "Clear", font = ("goudy old style", 15, "bold"), bg = "#607d8b", fg = "white", cursor = "hand2")
         self.btn_clear.place(x = 510, y = 400, width = 110, height = 40)
+
+        # Search Panel
+        self.var_search = StringVar()
+        lbl_search_courseName = Label(self.root, text = "Course Name:", font = ("goudy old style", 15, "bold"), bg = "white").place(x = 720, y = 60)
+        txt_search_courseName = Entry(self.root, textvariable = self.var_search, font = ("goudy old style", 15, "bold"), bg = "lightyellow").place(x = 870, y = 60, width = 180)
+
+        btn_search = Button(self.root, text = "Search", font = ("goudy old style", 15, "bold"), bg = "#03a9f4", fg = "white", cursor = "hand2").place(x = 1070, y = 60, width = 120, height = 28)
+
+        # Content
+        self.C_Frame = Frame(self.root, bd = 2, relief = RIDGE)
+        self.C_Frame.place(x = 720, y = 100, width = 470, height = 340)
+
+        scrolly = Scrollbar(self.C_Frame, orient = VERTICAL)
+        scrollx = Scrollbar(self.C_Frame, orient = HORIZONTAL)
+
+        self.courseTable = ttk.Treeview(self.C_Frame, columns = ("cid", "name", "duration", "charges", "description"), xscrollcommand = scrollx.set, yscrollcommand = scrolly.set)
+
+        scrollx.pack(side = BOTTOM, fill = X)
+        scrolly.pack(side = RIGHT, fill = Y)
+        scrollx.config(command = self.courseTable.xview)
+        scrolly.config(command = self.courseTable.yview)
+
+        self.courseTable.heading("cid", text = "Course ID")
+        self.courseTable.heading("name", text = "Name")
+        self.courseTable.heading("duration", text = "Duration")
+        self.courseTable.heading("charges", text = "Charges")
+        self.courseTable.heading("description", text = "Description")
+
+        self.courseTable["show"] = "headings"
+
+        self.courseTable.column("cid", width = 100)
+        self.courseTable.column("name", width = 100)
+        self.courseTable.column("duration", width = 100)
+        self.courseTable.column("charges", width = 100)
+        self.courseTable.column("description", width = 150)
+
+        self.courseTable.pack(fill = BOTH, expand = 1)
 
 if __name__ == "__main__":
     root = Tk()
