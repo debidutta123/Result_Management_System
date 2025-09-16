77d5df745a4f94be6ad9c9010054eff18b72745c
debidutta123 <165667866+debidutta123@users.noreply.github.com>
2025-09-16 20:43:58 +0530

    first commit

---
+++ dashboard.py
@@ -0,0 +1,42 @@
+from tkinter import *
+from PIL import Image, ImageTk
+
+class RMS:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("Result Management System")
+        self.root.geometry("1280x700+0+0")
+        self.root.config(bg="white")
+
+        # Icons
+        self.logo_dash = ImageTk.PhotoImage(file = "images/logo_p.png")
+
+        # Title
+        title = Label(self.root, text = "Student Result Management System", padx = 10, compound = LEFT, image = self.logo_dash, font = ("goudy old style", 20, "bold"), bg = "#033054", fg = "white").place(x = 0, y = 0, relwidth = 1, height = 50)
+
+        # Menu
+        M_Frame = LabelFrame(self.root, text = "Menus", font = ("Times new roman", 15), bg = "white")
+        M_Frame.place(x = 10, y = 60, width = 1260, height = 80)
+
+        # Buttons
+        btn_course = Button(M_Frame, text = "Course", font = ("goudy old style", 15, "bold"), bg = "#0b5377", fg = "white", cursor = "hand2").place(x = 20, y = 5, width = 195, height = 40)
+        btn_student = Button(M_Frame, text = "Student", font = ("goudy old style", 15, "bold"), bg = "#0b5377", fg = "white", cursor = "hand2").place(x = 225, y = 5, width = 195, height = 40)
+        btn_result = Button(M_Frame, text = "Result", font = ("goudy old style", 15, "bold"), bg = "#0b5377", fg = "white", cursor = "hand2").place(x = 430, y = 5, width = 195, height = 40)
+        btn_view = Button(M_Frame, text = "View Student Results", font = ("goudy old style", 15, "bold"), bg = "#0b5377", fg = "white", cursor = "hand2").place(x = 635, y = 5, width = 195, height = 40)
+        btn_logout = Button(M_Frame, text = "Logout", font = ("goudy old style", 15, "bold"), bg = "#0b5377", fg = "white", cursor = "hand2").place(x = 840, y = 5, width = 195, height = 40)
+        btn_exit = Button(M_Frame, text = "Exit", font = ("goudy old style", 15, "bold"), bg = "#0b5377", fg = "white", cursor = "hand2").place(x = 1045, y = 5, width = 195, height = 40)
+
+        # Content
+        self.bg_img = Image.open("images/bg.png")
+        self.bg_img = self.bg_img.resize((850, 350), Image.Resampling.LANCZOS)
+        self.bg_img = ImageTk.PhotoImage(self.bg_img)
+
+        self.lbl_bg = Label(self.root, image = self.bg_img).place(x = 400, y = 180, width = 850, height = 350)
+
+        # Footer
+        footer = Label(self.root, text = "SRMS-Student Result Management System\nContact Us For Any Technical Issue: sdebidutta2020@gmail.com", font = ("goudy old style", 12), bg = "#262626", fg = "white").pack(side = BOTTOM, fill = X)  
+
+if __name__ == "__main__":
+    root = Tk()
+    obj = RMS(root)
+    root.mainloop()
