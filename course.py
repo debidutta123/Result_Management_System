ce8ab1c37014a0488489e261993cf3284ad947b8
debidutta123 <165667866+debidutta123@users.noreply.github.com>
2025-09-17 01:18:26 +0530

    ok

--- course.py
+++ course.py
@@ -1,6 +1,7 @@
+import sqlite3
 from tkinter import *
-from tkinter import ttk
 from PIL import Image, ImageTk
+from tkinter import ttk, messagebox
 
 class Course:
     def __init__(self, root):
@@ -33,13 +34,13 @@ class Course:
         self.txt_description.place(x = 150, y = 180, width = 500, height = 130)
 
         # Buttons
-        self.btn_add = Button(self.root, text = "Save", font = ("goudy old style", 15, "bold"), bg = "#2196f3", fg = "white", cursor = "hand2")
+        self.btn_add = Button(self.root, text = "Save", font = ("goudy old style", 15, "bold"), bg = "#2196f3", fg = "white", cursor = "hand2", command = self.add)
         self.btn_add.place(x = 150, y = 400, width = 110, height = 40)
-        self.btn_update = Button(self.root, text = "Update", font = ("goudy old style", 15, "bold"), bg = "#4caf50", fg = "white", cursor = "hand2")
+        self.btn_update = Button(self.root, text = "Update", font = ("goudy old style", 15, "bold"), bg = "#4caf50", fg = "white", cursor = "hand2", command = self.update)
         self.btn_update.place(x = 270, y = 400, width = 110, height = 40)
-        self.btn_delete = Button(self.root, text = "Delete", font = ("goudy old style", 15, "bold"), bg = "#f44336", fg = "white", cursor = "hand2")
+        self.btn_delete = Button(self.root, text = "Delete", font = ("goudy old style", 15, "bold"), bg = "#f44336", fg = "white", cursor = "hand2", command = self.delete)
         self.btn_delete.place(x = 390, y = 400, width = 110, height = 40)
-        self.btn_clear = Button(self.root, text = "Clear", font = ("goudy old style", 15, "bold"), bg = "#607d8b", fg = "white", cursor = "hand2")
+        self.btn_clear = Button(self.root, text = "Clear", font = ("goudy old style", 15, "bold"), bg = "#607d8b", fg = "white", cursor = "hand2", command = self.clear)
         self.btn_clear.place(x = 510, y = 400, width = 110, height = 40)
 
         # Search Panel
@@ -47,7 +48,7 @@ class Course:
         lbl_search_courseName = Label(self.root, text = "Course Name:", font = ("goudy old style", 15, "bold"), bg = "white").place(x = 720, y = 60)
         txt_search_courseName = Entry(self.root, textvariable = self.var_search, font = ("goudy old style", 15, "bold"), bg = "lightyellow").place(x = 870, y = 60, width = 180)
 
-        btn_search = Button(self.root, text = "Search", font = ("goudy old style", 15, "bold"), bg = "#03a9f4", fg = "white", cursor = "hand2").place(x = 1070, y = 60, width = 120, height = 28)
+        btn_search = Button(self.root, text = "Search", font = ("goudy old style", 15, "bold"), bg = "#03a9f4", fg = "white", cursor = "hand2", command = self.search).place(x = 1070, y = 60, width = 120, height = 28)
 
         # Content
         self.C_Frame = Frame(self.root, bd = 2, relief = RIDGE)
@@ -78,6 +79,122 @@ class Course:
         self.courseTable.column("description", width = 150)
 
         self.courseTable.pack(fill = BOTH, expand = 1)
+
+        self.courseTable.bind("<ButtonRelease-1>", self.get_data)
+        self.show()
+
+    def show(self):
+        con = sqlite3.connect(database = r'rms.db')
+        cur = con.cursor()
+        try:
+            cur.execute("SELECT * FROM course")
+            rows = cur.fetchall()
+            self.courseTable.delete(*self.courseTable.get_children())
+            for row in rows:
+                self.courseTable.insert('', END, values = row)
+        except Exception as ex:
+            messagebox.showerror("Error!", f"Error Due To: {str(ex)}.", parent = self.root)
+
+    def search(self):
+        con = sqlite3.connect(database = r'rms.db')
+        cur = con.cursor()
+        try:
+            cur.execute(f"SELECT * FROM course WHERE name LIKE '%{self.var_search.get()}%'")
+            rows = cur.fetchall()
+            self.courseTable.delete(*self.courseTable.get_children())
+            for row in rows:
+                self.courseTable.insert('', END, values = row)
+        except Exception as ex:
+            messagebox.showerror("Error!", f"Error Due To: {str(ex)}.", parent = self.root)
+
+    def add(self):
+        con = sqlite3.connect(database = r'rms.db')
+        cur = con.cursor()
+        try:
+            if self.var_course.get() == "":
+                messagebox.showerror("Error!", "Course Name Must Be Required.", parent = self.root)
+            else:
+                cur.execute("SELECT * FROM course WHERE name = ?", (self.var_course.get(),))
+                row = cur.fetchone()
+                if row != None:
+                    messagebox.showerror("Error!", "Course Name Already Present. Try Different!", parent = self.root)
+                else:
+                    cur.execute("INSERT INTO course (name, duration, charges, description) VALUES (?, ?, ?, ?)", (
+                                    self.var_course.get(),
+                                    self.var_duration.get(),
+                                    self.var_charges.get(),
+                                    self.txt_description.get('1.0', END)
+                                ))
+                    con.commit()
+                    messagebox.showinfo("Success!", "Course Added Successfully.", parent = self.root)
+                    self.show()
+        except Exception as ex:
+            messagebox.showerror("Error!", f"Error Due To: {str(ex)}.", parent = self.root)
+
+    def update(self):
+        con = sqlite3.connect(database = r'rms.db')
+        cur = con.cursor()
+        try:
+            if self.var_course.get() == "":
+                messagebox.showerror("Error!", "Course Name Must Be Required.", parent = self.root)
+            else:
+                cur.execute("SELECT * FROM course WHERE name = ?", (self.var_course.get(),))
+                row = cur.fetchone()
+                if row == None:
+                    messagebox.showerror("Error!", "Select course from list.", parent = self.root)
+                else:
+                    cur.execute("UPDATE course set duration = ?, charges = ?, description = ? WHERE name = ?", (
+                                    self.var_duration.get(),
+                                    self.var_charges.get(),
+                                    self.txt_description.get('1.0', END),
+                                    self.var_course.get()
+                                ))
+                    con.commit()
+                    messagebox.showinfo("Success!", "Course Updated Successfully.", parent = self.root)
+                    self.show()
+        except Exception as ex:
+            messagebox.showerror("Error!", f"Error Due To: {str(ex)}.", parent = self.root)
+
+    def delete(self):
+        con = sqlite3.connect(database = r'rms.db')
+        cur = con.cursor()
+        try:
+            if self.var_course.get() == "":
+                messagebox.showerror("Error!", "Course Name Must Be Required.", parent = self.root)
+            else:
+                cur.execute("SELECT * FROM course WHERE name = ?", (self.var_course.get(),))
+                row = cur.fetchone()
+                if row == None:
+                    messagebox.showerror("Error!", "Please, Select course from the list first.", parent = self.root)
+                else:
+                    op = messagebox.askyesno("Confirm!", "Do you really want to delete?", parent = self.root)
+                    if op == True:
+                        cur.execute("DELETE FROM course WHERE name = ?", (self.var_course.get(),))
+                        con.commit()
+                        messagebox.showinfo("Delete!", "Course Deleted Successfully.", parent = self.root)
+                        self.clear()
+        except Exception as ex:
+            messagebox.showerror("Error!", f"Error Due To: {str(ex)}.", parent = self.root)
+
+    def clear(self):
+        self.show()
+        self.var_course.set("")
+        self.var_duration.set("")
+        self.var_charges.set("")
+        self.var_search.set("")
+        self.txt_description.delete('1.0', END)
+        self.txt_courseName.config(state = NORMAL)
+
+    def get_data(self, ev):
+        self.txt_courseName.config(state = 'readonly')
+        r = self.courseTable.focus()
+        content = self.courseTable.item(r)
+        row = content['values']
+        self.var_course.set(row[1])
+        self.var_duration.set(row[2])
+        self.var_charges.set(row[3])
+        self.txt_description.delete('1.0', END)
+        self.txt_description.insert(END, row[4])
 
 if __name__ == "__main__":
     root = Tk()
---
+++ create_db.py
@@ -0,0 +1,34 @@
+import sqlite3
+
+def create_db():
+    con = sqlite3.connect('rms.db')
+    cur = con.cursor()
+    
+    # Create tables
+    cur.execute('''CREATE TABLE IF NOT EXISTS course (
+                    cid INTEGER PRIMARY KEY AUTOINCREMENT,
+                    name TEXT NOT NULL,
+                    duration TEXT NOT NULL,
+                    charges TEXT NOT NULL,
+                    description TEXT
+                )''')
+    
+    # cur.execute('''CREATE TABLE IF NOT EXISTS student (
+    #                 id INTEGER PRIMARY KEY AUTOINCREMENT,
+    #                 name TEXT NOT NULL,
+    #                 email TEXT NOT NULL UNIQUE,
+    #                 course_id INTEGER,
+    #                 FOREIGN KEY(course_id) REFERENCES course(id)
+    #             )''')
+    
+    # cur.execute('''CREATE TABLE IF NOT EXISTS result (
+    #                 id INTEGER PRIMARY KEY AUTOINCREMENT,
+    #                 student_id INTEGER,
+    #                 subject TEXT NOT NULL,
+    #                 marks REAL NOT NULL,
+    #                 FOREIGN KEY(student_id) REFERENCES student(id)
+    #             )''')
+    
+    con.commit()
+    con.close()
+create_db()
